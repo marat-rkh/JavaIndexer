105978ea51b0fc8321224eed5bd247b8e0f97dd9
Marat Habibullin <maratx387@gmail.com>
2014-09-29 01:21:11 +0400

    Example program has been changed

--- src/indexer/Main.java
+++ src/indexer/Main.java
@@ -1,5 +1,6 @@
 package indexer;
 
+import indexer.exceptions.InconsistentIndexException;
 import indexer.exceptions.IndexClosedException;
 import indexer.tokenizer.Word;
 import indexer.tokenizer.WordsTokenizer;
@@ -18,6 +19,7 @@ public class Main {
         try (FSIndexer fsIndexer = new FSIndexer(new WordsTokenizer());
              BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             while (true) {
+                System.out.print("Enter command: ");
                 String input = br.readLine();
                 String[] command = input.split(" ");
                 switch (command[0]) {
@@ -43,39 +45,45 @@ public class Main {
                 }
             }
         } catch (IOException e) {
-            System.out.println("Error occurred. Details: " + e.getMessage());
+            System.out.println("IO error occurred. Details: " + e.getMessage());
         } catch (IndexClosedException e) {
             System.out.println("Strange situation - index has been closed");
+        } catch (InconsistentIndexException e) {
+            System.out.println("Inconsistent index error, details: " + e.getMessage());
         }
     }
 
     private static void showHelp() {
         final String help = "Commands:\n" +
-                "a <file_or_dir_path> - add file or dir to index" +
-                "r <file_or_dir_path> - remove file or dir from index" +
-                "s <word>             - get list of files containing <word>" +
-                "c <file_path>        - check if index contains file" +
-                "h                    - show this help" +
-                "q                    - finish work";
+                "a <file_or_dir_path> - add file or dir to index\n" +
+                "r <file_or_dir_path> - remove file or dir from index\n" +
+                "s <word>             - get list of files containing <word>\n" +
+                "c <file_path>        - check if index contains file\n" +
+                "h                    - show this help\n" +
+                "q                    - finish work\n\n";
         System.out.println(help);
     }
 
-    private static void addCommand(FSIndexer fsIndexer, String file) throws IndexClosedException {
+    private static void addCommand(FSIndexer fsIndexer, String file)
+            throws IndexClosedException, IOException, InconsistentIndexException {
         fsIndexer.add(file);
     }
 
-    private static void removeCommand(FSIndexer fsIndexer, String file) throws IndexClosedException {
+    private static void removeCommand(FSIndexer fsIndexer, String file)
+            throws IndexClosedException, IOException, InconsistentIndexException {
         fsIndexer.remove(file);
     }
 
-    private static void searchCommand(FSIndexer fsIndexer, String what) throws IndexClosedException {
+    private static void searchCommand(FSIndexer fsIndexer, String what)
+            throws IndexClosedException, InconsistentIndexException {
         List<String> files = fsIndexer.search(new Word(what));
         for(String f : files) {
             System.out.println(f);
         }
     }
 
-    private static void containsCommand(FSIndexer fsIndexer, String file) throws IndexClosedException {
+    private static void containsCommand(FSIndexer fsIndexer, String file)
+            throws IndexClosedException, InconsistentIndexException {
         System.out.println(fsIndexer.containsFile(file));
     }
 
