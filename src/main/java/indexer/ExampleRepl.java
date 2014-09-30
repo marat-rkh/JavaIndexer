ef43b91c53b51aa752a6f0e585413a7946e07c1a
Marat Habibullin <maratx387@gmail.com>
2014-09-30 19:35:34 +0400

    Repl now can exec queries asynchronously

---
+++ src/main/java/indexer/ExampleRepl.java
@@ -0,0 +1,208 @@
+package indexer;
+
+import indexer.exceptions.InconsistentIndexException;
+import indexer.exceptions.IndexClosedException;
+import indexer.tokenizer.Word;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by mrx on 30.09.14.
+ */
+public class ExampleRepl {
+    private final List<Thread> execThreads = new LinkedList<>();
+    private final Queue<String> resultsQueue = new ConcurrentLinkedQueue<>();
+    private final FSIndexer fsIndexer;
+    private final AtomicInteger lastCommandId = new AtomicInteger(0);
+
+    private boolean isInconsistentIndexException = false;
+    private String inconsistentIndexMsg = "";
+    private boolean isIndexClosedException = false;
+
+    public ExampleRepl(FSIndexer fsIndexer) {
+        this.fsIndexer = fsIndexer;
+    }
+
+    public void start(String commandsFilePath) {
+        showHelp();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getStream(commandsFilePath)))) {
+            while (true) {
+                checkState();
+                removeStoppedThreads();
+                String[] command = readCommand(br);
+                if (command[0].equals("p")) {
+                    printCollectedResults();
+                } else if (command[0].equals("h")) {
+                    showHelp();
+                } else if (command[0].equals("q")) {
+                    return;
+                } else if (command.length >= 2) {
+                    IndexCommandsRunner runner = new IndexCommandsRunner(lastCommandId.incrementAndGet(),
+                                                                         command[0], command[1]);
+                    Thread execThread = new Thread(runner);
+                    execThread.start();
+                    execThreads.add(execThread);
+                    System.out.println("Command #" + lastCommandId.get() + " is queued");
+                    printCollectedResults();
+                } else {
+                    showUnknownCommandMsg();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("IO error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Waiting active tasks to complete...");
+            for(Thread et : execThreads) {
+                try {
+                    et.join();
+                } catch (InterruptedException e) {
+                }
+            }
+            System.out.println("Done");
+        }
+    }
+
+    private void showHelp() {
+        final String help = "Commands:\n" +
+                "a <file_or_dir_path> - add file or dir to index\n" +
+                "r <file_or_dir_path> - remove file or dir from index\n" +
+                "s <word>             - get list of files containing <word>\n" +
+                "c <file_path>        - check if index contains file\n" +
+                "p                    - show previous commands results\n" +
+                "h                    - show this help\n" +
+                "q                    - finish work";
+        System.out.println(help);
+    }
+
+    private InputStream getStream(String commandsFilePath) throws FileNotFoundException {
+        if(commandsFilePath == null) {
+            return System.in;
+        }
+        return new FileInputStream(commandsFilePath);
+    }
+
+    private void checkState() throws Exception {
+        if(isInconsistentIndexException) {
+            throw new Exception("Inconsistent index error, details: " + inconsistentIndexMsg);
+        } else if(isIndexClosedException) {
+            throw new Exception("Strange situation - index has been closed");
+        }
+    }
+
+    private void removeStoppedThreads() {
+        Iterator<Thread> it = execThreads.iterator();
+        while (it.hasNext()) {
+            Thread et = it.next();
+            if(!et.isAlive()) {
+                it.remove();
+            }
+        }
+    }
+
+    private String[] readCommand(BufferedReader br) throws IOException {
+        System.out.println("\n$Enter command:");
+        String input = br.readLine();
+        return input.split(" ", 2);
+    }
+
+    private void printCollectedResults() {
+        System.out.println("Previous commands results:");
+        if(resultsQueue.size() != 0) {
+            for (String r : resultsQueue) {
+                System.out.println(r);
+            }
+        } else {
+            System.out.println("no results");
+        }
+    }
+
+    private void showUnknownCommandMsg() {
+        resultsQueue.offer("Unknown command");
+    }
+
+    private class IndexCommandsRunner implements Runnable {
+        private final int cmdNumber;
+        private final String cmd;
+        private final String arg;
+
+        public IndexCommandsRunner(int cmdNumber, String cmd, String arg) {
+            this.cmdNumber = cmdNumber;
+            this.cmd = cmd;
+            this.arg = arg;
+        }
+
+        @Override
+        public void run() {
+            try {
+                switch (cmd) {
+                    case "a":
+                        addCommand();
+                        break;
+                    case "r":
+                        removeCommand();
+                        break;
+                    case "s":
+                        searchCommand();
+                        break;
+                    case "c":
+                        containsCommand();
+                        break;
+                    default:
+                        showUnknownCommandMsg();
+                }
+            } catch (InconsistentIndexException e) {
+                isInconsistentIndexException = true;
+                inconsistentIndexMsg = e.getMessage();
+            } catch (IndexClosedException e) {
+                isIndexClosedException = true;
+            }
+        }
+
+        private void addCommand()
+                throws IndexClosedException, InconsistentIndexException {
+            try {
+                fsIndexer.add(arg);
+                resultsQueue.offer("#" + cmdNumber + "-Added: " + arg);
+            } catch (IOException e) {
+                resultsQueue.offer("#" + cmdNumber + "-File not added: " + arg + "\nReason: " + e.getMessage());
+            }
+        }
+
+        private void removeCommand()
+                throws IndexClosedException, InconsistentIndexException {
+            try {
+                fsIndexer.remove(arg);
+                resultsQueue.offer("#" + cmdNumber + "-Removed: " + arg);
+            } catch (IOException e) {
+                resultsQueue.offer("#" + cmdNumber + "-File not removed: " + arg + "\nReason: " + e.getMessage());
+            }
+        }
+
+        private void searchCommand()
+                throws IndexClosedException, InconsistentIndexException {
+            List<String> files = fsIndexer.search(new Word(arg));
+            if(files != null && files.size() != 0) {
+                String searchRes = "";
+                for (String f : files) {
+                    searchRes += (f + "\n");
+                }
+                resultsQueue.offer(searchRes);
+            } else {
+                resultsQueue.offer("#" + cmdNumber + "-No files found");
+            }
+        }
+
+        private void containsCommand()
+                throws IndexClosedException, InconsistentIndexException {
+            resultsQueue.offer("#" + cmdNumber + "-Contains " + arg + ": " + fsIndexer.containsFile(arg));
+        }
+    }
+}
--- src/main/java/indexer/Main.java
+++ src/main/java/indexer/Main.java
@@ -13,114 +13,11 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
-        showHelp();
-        try (FSIndexer fsIndexer = new FSIndexer(new WordsTokenizer(), System.out);
-             BufferedReader br = new BufferedReader(new InputStreamReader(getStream(args)))) {
-            while (true) {
-                System.out.println("Enter command:");
-                String input = br.readLine();
-                String[] command = input.split(" ", 2);
-                if (command[0].equals("h")) {
-                    showHelp();
-                } else if (command[0].equals("q")) {
-                    return;
-                } else if (command.length >= 2) {
-                    handleCommandWithArg(fsIndexer, command);
-                } else {
-                    showUnknownCommandMsg();
-                }
-            }
+        try (FSIndexer fsIndexer = new FSIndexer(new WordsTokenizer(), System.out)) {
+            ExampleRepl repl = new ExampleRepl(fsIndexer);
+            repl.start(args.length == 0 ? null : args[0]);
         } catch (IOException e) {
-            System.out.println("IO error: " + e.getMessage());
-        } catch (IndexClosedException e) {
-            System.out.println("Strange situation - index has been closed");
-        } catch (InconsistentIndexException e) {
-            System.out.println("Inconsistent index error, details: " + e.getMessage());
+            System.out.println("Index creation error: " + e.getMessage());
         }
-    }
-
-    private static InputStream getStream(String[] args) throws FileNotFoundException {
-        if(args.length == 0) {
-            return System.in;
-        }
-        return new FileInputStream(args[0]);
-    }
-
-    private static void showHelp() {
-        final String help = "Commands:\n" +
-                "a <file_or_dir_path> - add file or dir to index\n" +
-                "r <file_or_dir_path> - remove file or dir from index\n" +
-                "s <word>             - get list of files containing <word>\n" +
-                "c <file_path>        - check if index contains file\n" +
-                "h                    - show this help\n" +
-                "q                    - finish work\n\n";
-        System.out.println(help);
-    }
-
-    private static void handleCommandWithArg(FSIndexer fsIndexer, String[] command)
-            throws IndexClosedException, InconsistentIndexException {
-        switch (command[0]) {
-            case "a":
-                addCommand(fsIndexer, command[1]);
-                break;
-            case "r":
-                removeCommand(fsIndexer, command[1]);
-                break;
-            case "s":
-                searchCommand(fsIndexer, command[1]);
-                break;
-            case "c":
-                containsCommand(fsIndexer, command[1]);
-                break;
-            default:
-                showUnknownCommandMsg();
-        }
-    }
-
-    private static void addCommand(FSIndexer fsIndexer, String file)
-            throws IndexClosedException, InconsistentIndexException {
-        System.out.println("Adding: " + file);
-        try {
-            fsIndexer.add(file);
-            System.out.println("Added: " + file);
-        } catch (IOException e) {
-            System.out.println("File not added: " + file);
-            System.out.println("Reason: " + e.getMessage());
-        }
-    }
-
-    private static void removeCommand(FSIndexer fsIndexer, String file)
-            throws IndexClosedException, InconsistentIndexException {
-        System.out.println("Removing: " + file);
-        try {
-            fsIndexer.remove(file);
-            System.out.println("Removed: " + file);
-        } catch (IOException e) {
-            System.out.println("File not removed: " + file);
-            System.out.println("Reason: " + e.getMessage());
-        }
-    }
-
-    private static void searchCommand(FSIndexer fsIndexer, String what)
-            throws IndexClosedException, InconsistentIndexException {
-        System.out.println("Searching: " + what);
-        List<String> files = fsIndexer.search(new Word(what));
-        if(files != null && files.size() != 0) {
-            for (String f : files) {
-                System.out.println(f);
-            }
-        } else {
-            System.out.println("No files found");
-        }
-    }
-
-    private static void containsCommand(FSIndexer fsIndexer, String file)
-            throws IndexClosedException, InconsistentIndexException {
-        System.out.println("Check contains: " + file);
-        System.out.println(fsIndexer.containsFile(file));
-    }
-
-    private static void showUnknownCommandMsg() {
-        System.out.println("Unknown command");
     }
 }
