8bb2dc3d1d4873eabe385a49046863b5504e8c6f
Marat Habibullin <maratx387@gmail.com>
2014-10-04 15:43:44 +0400

    Commands from input file are not supported more

--- src/main/java/indexer/Main.java
+++ src/main/java/indexer/Main.java
@@ -3,7 +3,6 @@ package indexer;
 import indexer.tokenizer.WordsTokenizer;
 import indexer.utils.ConsoleLogger;
 import indexer.utils.ConsoleReadWriter;
-import indexer.utils.FileReadWriter;
 import indexer.utils.ReadWriter;
 
 import java.io.IOException;
@@ -13,20 +12,12 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        String commandsFile = args.length == 0 ? null : args[0];
-        try (ReadWriter readWriter = getReadWriter(commandsFile);
+        try (ReadWriter readWriter = new ConsoleReadWriter();
              FSIndexer fsIndexer = Indexers.newSimpleFsIndexer(new WordsTokenizer(), new ConsoleLogger(readWriter))) {
             ExampleRepl repl = new ExampleRepl(fsIndexer, readWriter);
             repl.start();
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
-    }
-
-    private static ReadWriter getReadWriter(String commandsFile) throws Exception {
-        if(commandsFile == null) {
-            return new ConsoleReadWriter();
-        }
-        return new FileReadWriter(commandsFile);
     }
 }
